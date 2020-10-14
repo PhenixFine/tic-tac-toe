959a1cda1813aabd58db8a74035dd2d3c06116d1
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-10-14 16:41:33 -0400

    Stage 3 of 5 for JetBrains Academy - Kotlin - Tic-Tac-Toe project. • now checks to see if there is a winner of a given game, if it's a draw, if what was given is impossible, and if the game is not complete ( stage 2 just had the given string printed as a game of tic tac toe ).

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,36 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    println("XOX\nXXO\nOXO")
+    val string = getString("Enter cells: ")
+    val cells = string.chunked(3)
+    val dashes = "-".repeat(9)
+    val xCount = string.count { it == 'X' }
+    val oCount = string.count { it == 'O' }
+    val xWins = checkWin(cells, 'X')
+    val oWins = checkWin(cells, 'O')
+    val empty = string.contains('_')
+
+    println(dashes)
+    cells.forEach { line -> print("| "); line.chunked(1).forEach { print("$it ") }; println("|") }
+    println(dashes)
+    when {
+        ((xWins && oWins) || (xCount - oCount).absoluteValue > 1) -> println("Impossible")
+        xWins -> println("X wins")
+        oWins -> println("O wins")
+        empty -> println("Game not finished")
+        else -> println("Draw")
+    }
+}
+
+fun checkWin(cells: List<String>, check: Char): Boolean {
+    cells.forEach { line -> if (line.all { it == check }) return true }
+    for (i in cells.indices) if (cells[0][i] == check && cells[1][i] == check && cells[2][i] == check) return true
+    if (cells[0][0] == check && cells[1][1] == check && cells[2][2] == check) return true
+    if (cells[0][2] == check && cells[1][1] == check && cells[2][0] == check) return true
+    return false
+}
+
+fun getString(text: String): String {
+    print(text)
+    return readLine()!!
 }
