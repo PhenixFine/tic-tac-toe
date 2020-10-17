3b102761a32cbfbbbb363b2262e7d084ee179b4c
Danny <64429863+PhenixFine@users.noreply.github.com>
2020-10-17 16:11:05 -0400

    Stage 4 of 5 for JetBrains Academy - Kotlin - Tic-Tac-Toe project. • program now has the user enter the games current state, and then asks the user for the next move. It then verifies if the next move is valid, if so it marks the spot with an X, if not, it re-asks the user for a valid move until one is entered. Steps for last stage were commented out, as Stage 4 mentioned that they would be needed for Stage 5. • main() - val cells was changed to a var. The printing of the game was moved to printGame(), and is called twice. The variable cells is changed with calling getCell(). Everything not currently needed was commented out. • getCell() - gets the next cell from the user, looping through a while statement until the user inputs a valid cell to change that is currently empty. The empty cell is then updated in cells, and returned. • printGame() - the printing steps from main() that was in stage 3 was moved to here. • isNumber() - verifies that a given string is a number.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,68 @@
-import kotlin.math.absoluteValue
-
 fun main() {
     val string = getString("Enter cells: ")
-    val cells = string.chunked(3)
+    var cells = string.chunked(3)
+    //val xCount = string.count { it == 'X' }
+    //val oCount = string.count { it == 'O' }
+    //val xWins = checkWin(cells, 'X')
+    //val oWins = checkWin(cells, 'O')
+    //val empty = string.contains('_')
+
+    printGame(cells)
+    cells = getCell(cells.toMutableList(), 'X')
+    printGame(cells)
+
+//    when {
+//        ((xWins && oWins) || (xCount - oCount).absoluteValue > 1) -> println("Impossible")
+//        xWins -> println("X wins")
+//        oWins -> println("O wins")
+//        empty -> println("Game not finished")
+//        else -> println("Draw")
+//    }
+}
+
+fun getCell(cells: MutableList<String>, char: Char): List<String> {
+    var numbers: List<String>
+    val range = 1..3
+
+    while (true) {
+        numbers = getString("Enter the coordinates: ").split(" ")
+        if (numbers.size != 2 || !isNumber(numbers[0]) || !isNumber(numbers[1]))
+            println("You should enter numbers!")
+        else if (!range.contains(numbers[0].toInt()) || !range.contains(numbers[1].toInt()))
+            println("Coordinates should be from 1 to 3!")
+        else if (cells[numbers[0].toInt() - 1][numbers[1].toInt() - 1] != '_')
+            println("This cell is occupied! Choose another one!")
+        else break
+    }
+
+    val i1 = numbers[0].toInt() - 1
+    val i2 = numbers[1].toInt() - 1
+
+    cells[i1] = (if (i2 > 0) cells[i1].substring(0, i2) else "") + char +
+            if (i2 < cells[i1].lastIndex) cells[i1].substring(i2 + 1) else ""
+
+    return cells
+}
+
+fun printGame(cells: List<String>) {
     val dashes = "-".repeat(9)
-    val xCount = string.count { it == 'X' }
-    val oCount = string.count { it == 'O' }
-    val xWins = checkWin(cells, 'X')
-    val oWins = checkWin(cells, 'O')
-    val empty = string.contains('_')
 
     println(dashes)
     cells.forEach { line -> print("| "); line.chunked(1).forEach { print("$it ") }; println("|") }
     println(dashes)
-    when {
-        ((xWins && oWins) || (xCount - oCount).absoluteValue > 1) -> println("Impossible")
-        xWins -> println("X wins")
-        oWins -> println("O wins")
-        empty -> println("Game not finished")
-        else -> println("Draw")
-    }
 }
 
-fun checkWin(cells: List<String>, check: Char): Boolean {
-    cells.forEach { line -> if (line.all { it == check }) return true }
-    for (i in cells.indices) if (cells[0][i] == check && cells[1][i] == check && cells[2][i] == check) return true
-    if (cells[0][0] == check && cells[1][1] == check && cells[2][2] == check) return true
-    if (cells[0][2] == check && cells[1][1] == check && cells[2][0] == check) return true
-    return false
-}
+//fun checkWin(cells: List<String>, check: Char): Boolean {
+//    cells.forEach { line -> if (line.all { it == check }) return true }
+//    for (i in cells.indices) if (cells[0][i] == check && cells[1][i] == check && cells[2][i] == check) return true
+//    if (cells[0][0] == check && cells[1][1] == check && cells[2][2] == check) return true
+//    if (cells[0][2] == check && cells[1][1] == check && cells[2][0] == check) return true
+//    return false
+//}
 
 fun getString(text: String): String {
     print(text)
     return readLine()!!
 }
+
+fun isNumber(number: String) = number.toIntOrNull() != null
